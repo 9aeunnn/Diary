130fffa5ca824e6d88bd29a981c19631e8cb8bbb
9aeunnn <gaeun_2020@naver.com>
2026-03-24 10:31:43 +0900

    Initial Commit

---
+++ Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by 김가은 on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge1
+//
+//  Created by 김가은 on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
